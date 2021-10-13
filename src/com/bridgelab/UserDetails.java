ac0a0b16b56da1226850dec0e25346cadfdad79b
[shivani tamboli] <[tambolishivani4@gmail.com]>
2021-10-13 08:56:20 +0530

    Added : to enter last  valid name

--- src/com/bridgelab/UserDetails.java
+++ src/com/bridgelab/UserDetails.java
@@ -2,6 +2,7 @@ package com.bridgelab;
 
 public class UserDetails {
 	String firstName;
+	String lastName;
 
 	public String getFirstName() {
 		return firstName;
@@ -9,5 +10,13 @@ public class UserDetails {
 
 	public void setfirstName(String firstName) {
 		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setlastName(String lastName) {
+		this.lastName = lastName;
 	}
 }
--- src/com/bridgelab/UserRegistration.java
+++ src/com/bridgelab/UserRegistration.java
@@ -5,7 +5,7 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class UserRegistration {
-	public static boolean validateFirstName(String firstName) {
+	public static boolean validateName(String firstName) {
 		String regex = "^[A-Z]{1}[a-z]{2,}$";
 		Pattern pattern = Pattern.compile(regex);
 		if (firstName.isEmpty())
@@ -24,11 +24,21 @@ public class UserRegistration {
 		UserRegistration registration = new UserRegistration();
 		System.out.println("Enter first name");
 		details.setfirstName(sc.next());
-		boolean flag = registration.validateFirstName(details.getFirstName());
+		boolean flag = registration.validateName(details.getFirstName());
 		if (flag)
 			System.out.println("Entered name is valid");
 		else
 			System.out.println("Entered name is invalid");
+
+		// validating last name.
+		System.out.println("Enter your last name");
+		details.setlastName(sc.next());
+
+		boolean lName = registration.validateName(details.getLastName());
+		if (lName)
+			System.out.println("Last name is valid");
+		else
+			System.out.println("Last name is invalid");
 		sc.close();
 	}
 }
